484e803fc8302c6eaf8c83b788bc23f41811e5ab
macintario <macintario@gmail.com>
2019-11-28 11:01:52 -0600

    inicio de suma de productos por regla de la cadena

--- ltxsympy.py
+++ ltxsympy.py
@@ -1,7 +1,16 @@
 from sympy import *
 from sympy.parsing.latex import parse_latex
-
-expresion = parse_latex(r"\frac{2}{\sqrt{9x-17}}")
-print(srepr(expresion))
-print(expresion)
-print(latex(diff(expresion)))
+salida = open("/tmp/solucion.txt","w")
+init_printing()
+x = var('x')
+h = var('h')
+f = parse_latex(r" 6x-\frac{3}{9x-6} ")
+fh=f.subs(x,x+h)
+drv=(fh-f)/h
+salida.write("$$\\displaystyle f(x)=%s$$<br/><br/>\n" %(latex(f)))
+salida.write("$$\\displaystyle f(x+h)=%s$$<br/><br/>\n" %latex(fh))
+salida.write("$$\\displaystyle \\frac{f(x+h)-f(x)}{h}=%s=%s $$\n <br/><br/>" % (latex(drv),latex(drv.factor())))
+drv=limit(drv,h,0)
+resultado=drv.apart()
+salida.write("$$\\displaystyle f'(x)=\\lim_{h\\to 0} \\frac{f(x+h)-f(x)}{h}=%s=%s$$<br/><br/>\n" % (latex(drv),latex(resultado)))
+print(resultado)
