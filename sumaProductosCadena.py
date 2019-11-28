e57b30c5c913991292ac82c642a845572e63d404
macintario <macintario@gmail.com>
2019-11-28 13:19:09 -0600

    suma de productos por regla de la cadena

--- sumaProductosCadena.py
+++ sumaProductosCadena.py
@@ -1,23 +1,34 @@
 from sympy import *
 from sympy.parsing.latex import parse_latex
-
+#Solución a derivada de y=uv+wz
 
 salida = open("/tmp/salida.txt","w")
 init_printing()
 x = var('x')
 u = var('u')
-f = parse_latex(r"\sqrt{u} ")
-g = parse_latex(r" \frac{3}{5}-\frac{2}{7}x")
-salida.write("$$f(x)=%s$$<br/>\n" % latex(f.subs(u,g)))
-salida.write("$$f(u)=%s$$<br/>\n" % latex(f))
-salida.write("$$g(x)=%s$$<br/>\n" % latex(g))
-df = diff(f)
-dg = diff(g)
-salida.write("$$\\frac{df}{du}=%s$$<br/>\n" % latex(df))
-salida.write("$$g(x)=%s$$<br/>\n" %latex(dg))
-result = df*dg
-salida.write("$$\\frac{df}{du}\\frac{du}{dx}=%s$$<br/>\n" % latex(result))
-result = result.subs(u,g)
-salida.write("$$\\frac{df}{dx}=%s=%s$$<br/>\n" % (latex(result), latex(result.cancel())))
-#salida.write(sp.latex(result))
+v = var('v')
+w = var('w')
+z = var('z')
+u = parse_latex(r"x")
+v = parse_latex(r"\sin(x)")
+w = parse_latex(r"x^2")
+z = parse_latex(r"\cos(x)")
+du = diff(u)
+dv = diff(v)
+dw = diff(w)
+dz = diff(z)
+salida.write("$$y=%s$$<br/>\n" % latex(u*v+w*z))
+salida.write("$$u=%s$$<br/>\n" % latex(u))
+salida.write("$$v=%s$$<br/>\n" % latex(v))
+salida.write("$$w=%s$$<br/>\n" % latex(w))
+salida.write("$$z=%s$$<br/>\n" % latex(z))
+#derivadas
+salida.write("$$u'=%s$$<br/>\n" % latex(du))
+salida.write("$$v'=%s$$<br/>\n" % latex(dv))
+salida.write("$$w'=%s$$<br/>\n" % latex(dw))
+salida.write("$$z'=%s$$<br/>\n" % latex(dz))
+
+result = du*v+dv*u+dw*z+dz*w
+salida.write("$$y'=(u\'v+v\'u)+(w\'z+z\'w)=[(%s) (%s) +(%s) (%s)]+[(%s) (%s)]+[(%s)(%s)]$$<br/>\n"% ( latex(du), latex(v),latex(dv), latex(u), latex(dw), latex(z),latex(dz), latex(w) ) )
+salida.write("$$y'=%s$$<br/>\n" % latex(result))
 salida.close()
